687c130cc26cb064549f474aa368a884d2ee240e
San4illa <san4illa@bk.ru>
2020-06-21 18:24:20 +0300

    Add RxJava3

--- app/src/main/java/com/san4illa/itunesalbums/ui/base/BasePresenter.kt
+++ app/src/main/java/com/san4illa/itunesalbums/ui/base/BasePresenter.kt
@@ -1,7 +1,14 @@
 package com.san4illa.itunesalbums.ui.base
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import moxy.MvpPresenter
 import moxy.MvpView
 
 open class BasePresenter<V : MvpView> : MvpPresenter<V>() {
+    protected val disposables = CompositeDisposable()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.clear()
+    }
 }
